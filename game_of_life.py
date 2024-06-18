949cbc9ddaf70814e192713473a6ee593d3a904f
dungatoro <71789541+dungatoro@users.noreply.github.com>
2024-06-18 19:42:01 +0100

    Update game_of_life.py

--- game_of_life.py
+++ game_of_life.py
@@ -26,7 +26,40 @@ grid = [
     [0,0,0,0,0],
 ]
 
+x, y = 0, 0
+pixel = 0
+
+def move():
+    global x, y
+    x += 1
+    if x == 5:
+        x, y = 0, y+1
+    if y == 5:
+        y = 0
+        
+display.show(grid_to_image(grid))
 while True:
-    display.show(grid_to_image(grid))
+    while button_a.is_pressed() and button_b.is_pressed():
+    
+
+        sleep(200)
+        grid = tick(grid)
+
+    if button_a.was_pressed():
+        display.set_pixel(x, y, pixel)
+        move()
+        pixel = display.get_pixel(x, y)
+
+    if button_b.was_pressed():
+        if pixel == 9:
+            display.set_pixel(x, y, 0)
+        else:
+            display.set_pixel(x, y, 9)
+            
+        move()
+        pixel = display.get_pixel(x, y)
+
+    display.set_pixel(x, y, 9)
     sleep(200)
-    grid = tick(grid)
+    display.set_pixel(x, y, 0)
+    sleep(200)
